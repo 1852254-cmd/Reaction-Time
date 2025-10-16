09a8f65acb49e303df95537ca529a3bef77902e6
Patrick O'Connell <1852254@fcpsschools.net>
2025-10-16 09:02:56 -0400

    updated
    Initial setup for a reaction time test game with classes and event handlers.

--- main.py
+++ main.py
@@ -1,1 +1,73 @@
 
+# reaction time test thingymajig
+
+# ---------------------------------
+# Instructions
+# ---------------------------------
+
+# try to catch the batons!
+
+# ---------------------------------
+# TODO
+# ---------------------------------
+
+# everything gang
+
+# ---------------------------------
+# Source Code
+# ---------------------------------
+from cmu_graphics import *
+from random import randrange
+import math
+
+# ---------------------------------
+# classes
+# ---------------------------------
+
+class Dropper:
+    batons = []
+    
+class Baton:
+    x,y = 0,0
+    
+class Grabber:
+    points = 0
+
+# ---------------------------------
+# app functions
+# ---------------------------------
+
+def onKeyHold(app, keys):
+    pass
+
+def onKeyPress(app, key):
+    pass
+
+def onMousePress(app, mouseX, mouseY):
+    pass
+    
+def onMouseRelease(app, mouseX, mouseY):
+    pass
+
+def onMouseDrag(app, mouseX, mouseY):
+    pass
+
+def onMouseMove(app, mouseX, mouseY):
+    pass
+
+def redrawAll(app):
+    pass
+    
+def onStep(app):
+    pass
+
+def resetApp(app):
+    pass
+
+def onAppStart(app):
+    resetApp(app)
+
+def main():
+    runApp()
+    
+main()
